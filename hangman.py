01309ce67895f8fcf37e77d2c829bd12a78b6d9c
aksharakoppula <aadya.koppula@gmail.com>
2026-03-18 19:09:56 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -10,13 +10,42 @@ load_words()
 word = random.choice(word_bank)
 
 word_spaces = []
-for num in range(len(word)-1):
+
+for num in range(len(word)):
     word_spaces.append("_")
     
-output = ""
-for i in range(len(word_spaces)):
-    output += word_spaces[i]
-    if i != len(word_spaces)-1:
-        output += " "
-    
-print(output)
+
+def show_progress(word_spaces):
+    output = ""    
+    for i in range(len(word_spaces)):
+        output += word_spaces[i]
+        if i != len(word_spaces) - 1:
+            output += " "
+    return output
+
+lives = 5
+
+def check_word(guess):
+    letter_count = 0
+    for i in range(len(word)):
+        if word[i] == guess:
+            word_spaces[i] = guess
+            letter_count += 1
+    return letter_count
+
+running = True
+
+# weird issue where it doesn't think we're passing in anythingggggg
+while running:
+    output = show_progress(word_spaces)
+    print(f"{output}\n")
+    guess = input("Enter a single letter guess: ").lower()
+    while not guess.isalpha() or len(guess) != 1:
+        guess = input("Please enter a valid guess: ")
+    count = check_word(guess)
+    if count == 0:
+        lives -=1
+        print(f"Your guess was not in the word, you have {lives} lives left\n")
+
+
+ 
